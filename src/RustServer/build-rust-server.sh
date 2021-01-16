adebad96b7b5b787bc50ecbc3745ee15935295fc
Kishan Bagaria <KishanBagaria@users.noreply.github.com>
2021-01-17 00:01:56 +0530

    build separate binaries for arm64 and x64

--- src/RustServer/build-rust-server.sh
+++ src/RustServer/build-rust-server.sh
@@ -9,16 +9,13 @@ cargo build --manifest-path src/RustServer/Cargo.toml --release
 FILE_PATH_X64=src/RustServer/target/release/rust_server
 FILE_PATH_ARM64=src/RustServer/target/aarch64-apple-darwin/release/rust_server
 
-# ls -lah $FILE_PATH_X64
-# strip $FILE_PATH_X64
-# ls -lah $FILE_PATH_X64
+ls -lah $FILE_PATH_X64
+strip $FILE_PATH_X64
+ls -lah $FILE_PATH_X64
 
-# ls -lah $FILE_PATH_ARM64
-# strip $FILE_PATH_ARM64
-# ls -lah $FILE_PATH_ARM64
+ls -lah $FILE_PATH_ARM64
+strip $FILE_PATH_ARM64
+ls -lah $FILE_PATH_ARM64
 
-lipo -create $FILE_PATH_ARM64 $FILE_PATH_X64 -output binaries/rust_server
-
-ls -lah binaries/rust_server
-strip binaries/rust_server
-ls -lah binaries/rust_server
+cp $FILE_PATH_ARM64 binaries/rust_server_arm64_macos
+cp $FILE_PATH_X64 binaries/rust_server_x64_macos
