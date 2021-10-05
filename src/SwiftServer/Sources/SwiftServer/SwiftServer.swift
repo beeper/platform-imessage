2b2f9d7fd0d7e47cb1d4c73cd2addfde36c44c48
kabiroberai <oberai.kabir@gmail.com>
2021-10-05 19:17:17 +0530

    update node-swift

--- src/SwiftServer/Sources/SwiftServer/SwiftServer.swift
+++ src/SwiftServer/Sources/SwiftServer/SwiftServer.swift
@@ -9,7 +9,7 @@ import Foundation
         guard let decoded = try AttributedStringDecoder.decodeAttributedString(from: data)
             else { return nil }
         return try decoded.map { frag in
-            let obj = try NodeObject(in: .current)
+            let obj = try NodeObject()
             try obj.define(properties: [
                 NodePropertyDescriptor(
                     name: "from",
@@ -36,7 +36,7 @@ import Foundation
         }
     }
 
-    init(context: NodeContext) throws {
+    init() throws {
         var _controller: MessagesController?
         func controller() throws -> MessagesController {
             guard let controller = _controller else {
@@ -52,15 +52,15 @@ import Foundation
                 return controller
             }
         }
-        func performAsync(with ctx: NodeContext, action: @escaping () throws -> Void) throws -> NodePromise {
-            let deferred = try NodePromise.Deferred(in: ctx)
+        func performAsync(_ action: @escaping () throws -> Void) throws -> NodePromise {
+            let deferred = try NodePromise.Deferred()
             let tsfn = try NodeThreadsafeFunction<Error?>(
-                asyncResourceName: "swift_server_perform", in: ctx
-            ) { ctx, err in
+                asyncResourceName: "swift_server_perform"
+            ) { err in
                 if let err = err {
-                    try deferred.reject(with: NodeError(code: "\(type(of: err))", message: "\(err)", in: ctx), in: ctx)
+                    try deferred.reject(with: NodeError(code: "\(type(of: err))", message: "\(err)"))
                 } else {
-                    try deferred.resolve(with: NodeUndefined(in: ctx), in: ctx)
+                    try deferred.resolve(with: NodeUndefined())
                 }
             }
             MessagesController.queue.async {
@@ -75,57 +75,57 @@ import Foundation
             return deferred.promise
         }
         exports = [
-            "init": try NodeFunction(in: context) { ctx, info in
+            "init": try NodeFunction { info in
                 if info.arguments.count == 1,
                    let isLoggingEnabled = try? info.arguments[0].as(NodeBool.self)?.bool() {
                     gIsLoggingEnabled = isLoggingEnabled
                 }
                 debugLog("initializing SwiftServer...")
-                return try performAsync(with: ctx) {
+                return try performAsync {
                     if _controller == nil {
                         _controller = try .init()
                     }
                 }
             },
-            "decodeAttributedString": try NodeFunction(in: context) { ctx, info in
+            "decodeAttributedString": try NodeFunction { info in
                 guard let buffer = try info.arguments.first?.as(NodeBuffer.self),
                       let decoded = try Self.decodeAttributedString(from: buffer.data()) else {
-                    return try NodeUndefined(in: ctx)
+                    return try NodeUndefined()
                 }
                 return decoded
             },
-            "createThread": try NodeFunction(in: context) { ctx, info in
+            "createThread": try NodeFunction { info in
                 guard info.arguments.count == 2,
                       let arr = try info.arguments[0].as(NodeArray.self),
                       let message = try info.arguments[1].as(NodeString.self)?.string() else {
-                    return try NodeUndefined(in: ctx)
+                    return try NodeUndefined()
                 }
                 let addresses = try (0..<arr.count()).compactMap {
-                    try arr[Double($0)].get(in: ctx).as(NodeString.self)?.string()
+                    try arr[Double($0)].get().as(NodeString.self)?.string()
                 }
-                return try performAsync(with: ctx) {
+                return try performAsync() {
                     try controller().createThread(addresses: addresses, message: message)
                 }
             },
-            "markRead": try NodeFunction(in: context) { ctx, info in
+            "markRead": try NodeFunction { info in
                 guard let guid = try info.arguments.first?.as(NodeString.self) else {
-                    return try NodeUndefined(in: ctx)
+                    return try NodeUndefined()
                 }
                 let guidString = try guid.string()
-                return try performAsync(with: ctx) {
+                return try performAsync {
                     try controller().markAsRead(guid: guidString)
                 }
             },
-            "sendTypingStatus": try NodeFunction(in: context) { ctx, info in
+            "sendTypingStatus": try NodeFunction { info in
                 guard info.arguments.count == 2,
                       let isTyping = try? info.arguments[0].as(NodeBool.self)?.bool(),
                       let address = try? info.arguments[1].as(NodeString.self)?.string()
-                else { return try NodeUndefined(in: ctx) }
-                return try performAsync(with: ctx) {
+                else { return try NodeUndefined() }
+                return try performAsync {
                     try controller().sendTypingStatus(isTyping, address: address)
                 }
             },
-            "watchThreadActivity": try NodeFunction(in: context) { ctx, info in
+            "watchThreadActivity": try NodeFunction { info in
                 let args: (String, (MessagesController.ActivityStatus) -> Void)?
                 if try info.arguments.count == 1 && info.arguments[0].as(NodeNull.self) != nil {
                     args = nil
@@ -134,16 +134,16 @@ import Foundation
                           let fn = try info.arguments[1].as(NodeFunction.self) {
                     let addressName = try address.string()
                     let tsfn = try NodeThreadsafeFunction<MessagesController.ActivityStatus>(
-                        asyncResourceName: "watch_imessage_callback", in: ctx
-                    ) { ctx, param in
-                        try fn(in: ctx, param.rawValue)
+                        asyncResourceName: "watch_imessage_callback"
+                    ) { param in
+                        try fn(param.rawValue)
                     }
                     args = (addressName, { try? tsfn($0) })
                 } else {
                     print("warning: Invalid args to watchThreadActivity")
                     args = nil
                 }
-                return try performAsync(with: ctx) {
+                return try performAsync {
                     let controller = try controller()
                     if let args = args {
                         try controller.observe(address: args.0, callback: args.1)
@@ -152,44 +152,44 @@ import Foundation
                     }
                 }
             },
-            "setReaction": try NodeFunction(in: context) { ctx, info in
+            "setReaction": try NodeFunction { info in
                 guard info.arguments.count == 4,
                       let guid = try? info.arguments[0].as(NodeString.self)?.string(),
                       let offset = try? info.arguments[1].as(NodeNumber.self)?.double(),
                       let reactionName = try? info.arguments[2].as(NodeString.self)?.string(),
                       let reaction = MessagesController.Reaction(rawValue: reactionName),
                       let on = try? info.arguments[3].as(NodeBool.self)?.bool() else {
-                    return try NodeUndefined(in: ctx)
+                    return try NodeUndefined()
                 }
-                return try performAsync(with: ctx) {
+                return try performAsync {
                     try controller().setReaction(guid: guid, offset: Int(offset), reaction: reaction, on: on)
                 }
             },
-            "sendTextMessage": try NodeFunction(in: context) { ctx, info in
+            "sendTextMessage": try NodeFunction { info in
                 guard info.arguments.count == 2,
                       let text = try? info.arguments[0].as(NodeString.self)?.string(),
                       let threadID = try? info.arguments[1].as(NodeString.self)?.string()
-                else { return try NodeUndefined(in: ctx) }
-                return try performAsync(with: ctx) {
+                else { return try NodeUndefined() }
+                return try performAsync {
                     try controller().sendTextMessage(text, threadID: threadID)
                 }
             },
-            "sendReply": try NodeFunction(in: context) { ctx, info in
+            "sendReply": try NodeFunction { info in
                 guard info.arguments.count == 2,
                       let guid = try? info.arguments[0].as(NodeString.self)?.string(),
                       let text = try? info.arguments[1].as(NodeString.self)?.string()
-                else { return try NodeUndefined(in: ctx) }
-                return try performAsync(with: ctx) {
+                else { return try NodeUndefined() }
+                return try performAsync {
                     try controller().sendReply(guid: guid, text: text)
                 }
             },
-            "dispose": try NodeFunction(in: context) { ctx, info in
+            "dispose": try NodeFunction { info in
                 debugLog("disposing SwiftServer...")
                 MessagesController.queue.sync {
                     _controller?.dispose()
                     _controller = nil
                 }
-                return try NodeUndefined(in: ctx)
+                return try NodeUndefined()
             }
         ]
     }
