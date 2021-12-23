e905aad84eeed34a6369362c56f48008e4243f41
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2021-12-24 03:10:36 +0530

    isSelectable: handle tweets

--- src/info.ts
+++ src/info.ts
@@ -5,6 +5,7 @@ import type { MessageWithExtra } from './mappers'
 const isSelectable = (message: MessageWithExtra) =>
   !message.attachments?.length
   && !message.links?.length
+  && !message.tweets?.length
   && typeof message.extra?.part === 'undefined'
 
 const info: PlatformInfo = {
