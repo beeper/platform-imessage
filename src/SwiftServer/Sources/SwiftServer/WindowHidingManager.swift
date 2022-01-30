815684b14d22ba5e5b4cfee0c5f1eeaf3460845a
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2022-01-30 23:20:10 +0530

    use CGSMoveWindowsToManagedSpace and create user space on 12.2+

--- src/SwiftServer/Sources/SwiftServer/WindowHidingManager.swift
+++ src/SwiftServer/Sources/SwiftServer/WindowHidingManager.swift
@@ -11,22 +11,58 @@ protocol WindowHidingManager {
     func appDeactivated(window: Accessibility.Element) throws
 }
 
+extension Space {
+    var isVisibleInMissionControl: Bool {
+        self.dockPID != Dock.getPID()
+    }
+}
+
+let CAN_USE_UNKNOWN_SPACE = !(ProcessInfo.processInfo.operatingSystemVersion.majorVersion >= 12 && ProcessInfo.processInfo.operatingSystemVersion.minorVersion >= 2)
 final class SpacesWindowHidingManager: WindowHidingManager {
     let canReuseApp = true
     var isValid = true
+
+    static func createOrGetInvisibleUserSpace() throws -> Space {
+        let allSpaces = try Space.list(.allSpaces)
+        let existing = allSpaces.filter {
+            guard let values = try? $0.values() else { return false }
+            guard let dict = values as? [String: AnyObject] else { return false }
+            let createdByUs = (dict["uuid"] as? String)?.hasPrefix("Texts") == true
+            let isSpaceKindUser = (dict["type"] as? UInt32) == Space.Kind.user.raw.rawValue
+            let isSameDock = (dict["dockPID"] as? pid_t) == Dock.getPID() // has to be the same dock instance or the space will be visible
+            if createdByUs, isSpaceKindUser, !isSameDock {
+                debugLog("space \($0.raw) was created by us and dock has been relaunched (it may be visible now)")
+                // hide/destroy apparently has no effect atm
+                // $0.hide()
+                // $0.destroy()
+            }
+            return createdByUs && isSpaceKindUser && isSameDock
+        }.first
+        if let existing = existing { debugLog("reusing existing space \(existing.raw)") }
+        return try existing ?? Space(newSpaceOfKind: .user)
+    }
+
     private var lastActiveDisplay: Display?
-    private let space: Space
+    private var _hiddenSpace: Space
+    private var hiddenSpace: Space {
+        get throws {
+            if CAN_USE_UNKNOWN_SPACE || !_hiddenSpace.isVisibleInMissionControl { return _hiddenSpace }
+            _hiddenSpace = try Self.createOrGetInvisibleUserSpace()
+            return _hiddenSpace
+        }
+    }
 
     init() throws {
-        space = try Space(newSpaceOfKind: .fullscreen)
-
+        debugLog("CAN_USE_UNKNOWN_SPACE \(CAN_USE_UNKNOWN_SPACE)")
+        _hiddenSpace = try CAN_USE_UNKNOWN_SPACE ? Space(newSpaceOfKind: .unknown) : Self.createOrGetInvisibleUserSpace()
         #if DEBUG
-        let existing = try Space.list()
-        debugLog("[spaces] \(existing.count) space(s)")
-        existing.forEach { $0.printAttributes() }
-        // existing.filter { (try? $0.name()) == "1FBF2F7F-57EC-56E5-521F-556A305D1A61" }.forEach {
-        //     $0.destroy()
-        // }
+        // the main space has an empty string as uuid/name and 1 as compat id
+        // "mission-control", "dock", "NotificationCenter", "SpacesBarWindowController", "SensorIndicators", "ControlCenter", "com.apple.loginUI", "AccessibilityVisualsSpace"
+        // are some unknown spaces not present in .allSpaces
+        let all = try Space.list(.allOSSpaces)
+        debugLog("[spaces] \(all.count) space(s)")
+        all.forEach { $0.printAttributes() }
+        // all.filter { (try? $0.name()) == "1FBF2F7F-57EC-56E5-521F-556A305D1A61" }.forEach { $0.destroy() }
         #endif
     }
 
@@ -51,44 +87,42 @@ final class SpacesWindowHidingManager: WindowHidingManager {
     }
 
     func mainWindowFetched(_ mainWindow: Accessibility.Element) throws {
-        lastActiveDisplay = try Self.moveWindow(mainWindow, to: space)
+        lastActiveDisplay = try Self.moveWindow(mainWindow, to: hiddenSpace)
     }
 
     func appActivated(window: Accessibility.Element) throws {
-        guard let space = try? lastActiveDisplay?.currentSpace() else {
+        guard let currentSpace = try? lastActiveDisplay?.currentSpace() else {
             debugLog("WindowHidingManager.appActivated: space not found")
             return
         }
         debugLog("WindowHidingManager.appActivated: moving window")
-        try window.window().moveToSpace(space)
+        try window.window().moveToSpace(currentSpace)
     }
 
     func appDeactivated(window: Accessibility.Element) throws {
         debugLog("WindowHidingManager.appDeactivated: moving window")
-        lastActiveDisplay = try Self.moveWindow(window, to: space)
+        lastActiveDisplay = try Self.moveWindow(window, to: hiddenSpace)
     }
 }
 
-final class RelaunchWindowHidingManager: WindowHidingManager {
-    let canReuseApp = false
-    var isValid = true
-    var invalidate = false
+// final class RelaunchWindowHidingManager: WindowHidingManager {
+//     let canReuseApp = false
+//     var isValid = true
+//     var invalidate = false
 
-    func mainWindowFetched(_ mainWindow: Accessibility.Element) throws {}
+//     func mainWindowFetched(_ mainWindow: Accessibility.Element) throws {}
 
-    func appActivated(window: Accessibility.Element) throws {
-        debugLog("WindowHidingManager.appActivated: invalidating")
-        invalidate = true
-    }
+//     func appActivated(window: Accessibility.Element) throws {
+//         debugLog("WindowHidingManager.appActivated: invalidating")
+//         invalidate = true
+//     }
 
-    func appDeactivated(window: Accessibility.Element) throws {
-        isValid = false
-        debugLog("WindowHidingManager.appDeactivated: invalidated")
-    }
-}
+//     func appDeactivated(window: Accessibility.Element) throws {
+//         isValid = false
+//         debugLog("WindowHidingManager.appDeactivated: invalidated")
+//     }
+// }
 
-let CAN_USE_SPACES_API = !(ProcessInfo.processInfo.operatingSystemVersion.majorVersion >= 12 && ProcessInfo.processInfo.operatingSystemVersion.minorVersion >= 2)
 func getBestWHM() throws -> WindowHidingManager {
-    debugLog("CAN_USE_SPACES_API \(CAN_USE_SPACES_API)")
-    return CAN_USE_SPACES_API ? try SpacesWindowHidingManager() : RelaunchWindowHidingManager()
+    try SpacesWindowHidingManager()
 }
