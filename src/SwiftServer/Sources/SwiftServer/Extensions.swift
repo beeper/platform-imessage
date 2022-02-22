b21ec565c1309d7ca5ea48e77fcff5bceb424c70
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2022-02-23 01:24:25 +0530

    silence warnings

--- src/SwiftServer/Sources/SwiftServer/Extensions.swift
+++ src/SwiftServer/Sources/SwiftServer/Extensions.swift
@@ -4,13 +4,13 @@ extension NSRunningApplication {
     func waitForLaunch(timeout seconds: TimeInterval = 5) throws {
         let start = Date()
         while !self.isFinishedLaunching {
-            debugLog("sleeping 0.1s for \(self.localizedName) to finish launching")
+            debugLog("sleeping 0.1s for \(String(describing: self.localizedName)) to finish launching")
             Thread.sleep(forTimeInterval: 0.1)
             if self.isTerminated {
-                throw ErrorMessage("\(self.localizedName) terminated")
+                throw ErrorMessage("\(String(describing: self.localizedName)) terminated")
             }
             if start.timeIntervalSinceNow < -seconds {
-                debugLog("assuming \(self.localizedName) has launched") // sometimes this gets stuck in an infinite loop
+                debugLog("assuming \(String(describing: self.localizedName)) has launched") // sometimes this gets stuck in an infinite loop
                 break
             }
         }
