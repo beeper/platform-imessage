1b919350193e03e35f416b88de1be4e7ac80232f
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2022-07-20 21:59:24 +0530

    wrap only NSUnarchiver.decodeObject in ExceptionCatcher
    https://iosdevelopers.slack.com/archives/C031X84F6/p1658330526704879?thread_ts=1658147279.256379&cid=C031X84F6
    
    "This is almost guaranteed to leave Swift objects partially initialized and alive on the heap, since the unwinding logic won't go through and run destructors for Swift objects. It is not valid to hop through any Swift stack frames when catching objective-c exceptions, that's the main reason Swift doesn't have this built-in"

--- src/SwiftServer/Sources/SwiftServer/AttributedStringDecoder.swift
+++ src/SwiftServer/Sources/SwiftServer/AttributedStringDecoder.swift
@@ -9,37 +9,34 @@ enum AttributedStringDecoder {
     }
 
     static func decodeAttributedString(from data: Data) throws -> [Fragment] {
+        let unarchiver = try NSUnarchiver(forReadingWith: data)
+            .orThrow(ErrorMessage("Couldn't create NSUnarchiver"))
+        let decoded = try ExceptionCatcher.catch { unarchiver.decodeObject() }
+        let nsStr = try (decoded as? NSAttributedString)
+            .orThrow(ErrorMessage("Decoded object type unknown"))
+        let string = nsStr.string
+
         var fragments: [Fragment] = []
-        try ExceptionCatcher.catch {
-            let unarchiver = try NSUnarchiver(forReadingWith: data)
-                .orThrow(ErrorMessage("Couldn't create NSUnarchiver"))
-            let decoded = try unarchiver.decodeTopLevelObject()
-            let nsStr = try (decoded as? NSAttributedString)
-                .orThrow(ErrorMessage("Decoded object type unknown"))
-            let string = nsStr.string
-
-
-            // https://github.com/apple/swift-corelibs-foundation/blob/b3b87b6328325b639032bdc92e384f33f0beef0e/Sources/Foundation/AttributedString/Conversion.swift#L222-L251
-            var cursor = string.startIndex
-            var curScalar = 0
-            nsStr.enumerateAttributes(
-                in: NSRange(location: 0, length: nsStr.length),
-                options: .longestEffectiveRangeNotRequired
-            ) { dict, range, _ in
-                let nextCursor = string.utf16.index(cursor, offsetBy: range.length)
-                let scalarLen = string.unicodeScalars.distance(from: cursor, to: nextCursor)
-                var attributes: [String: Any] = [:]
-                for (key, value) in dict {
-                    attributes[key.rawValue] = value
-                }
-                fragments.append(Fragment(
-                    text: string[cursor..<nextCursor],
-                    scalarRange: curScalar..<(curScalar + scalarLen),
-                    attributes: attributes
-                ))
-                cursor = nextCursor
-                curScalar += scalarLen
+        // https://github.com/apple/swift-corelibs-foundation/blob/b3b87b6328325b639032bdc92e384f33f0beef0e/Sources/Foundation/AttributedString/Conversion.swift#L222-L251
+        var cursor = string.startIndex
+        var curScalar = 0
+        nsStr.enumerateAttributes(
+            in: NSRange(location: 0, length: nsStr.length),
+            options: .longestEffectiveRangeNotRequired
+        ) { dict, range, _ in
+            let nextCursor = string.utf16.index(cursor, offsetBy: range.length)
+            let scalarLen = string.unicodeScalars.distance(from: cursor, to: nextCursor)
+            var attributes: [String: Any] = [:]
+            for (key, value) in dict {
+                attributes[key.rawValue] = value
             }
+            fragments.append(Fragment(
+                text: string[cursor..<nextCursor],
+                scalarRange: curScalar..<(curScalar + scalarLen),
+                attributes: attributes
+            ))
+            cursor = nextCursor
+            curScalar += scalarLen
         }
         return fragments
     }
