4708579b6e9fcdcaf7f6a1a44f103add500ac6f2
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2022-08-28 18:44:05 +0530

    isSelectable: handle emoji only messages

--- src/common-util.ts
+++ src/common-util.ts
@@ -1,8 +1,9 @@
+import { isEmojiOrSpacesOnlyString } from '@textshq/platform-sdk/dist/emoji'
 import type { MessageWithExtra } from './mappers'
 
-// todo handle emoji only messages
 export const isSelectable = (message: MessageWithExtra) =>
   (!message.attachments?.length
     && !message.links?.length
     && !message.tweets?.length
-    && typeof message.extra?.part === 'undefined')
+    && typeof message.extra?.part === 'undefined'
+    && !isEmojiOrSpacesOnlyString(message.text))
