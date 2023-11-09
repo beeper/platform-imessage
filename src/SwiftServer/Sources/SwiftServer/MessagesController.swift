c3c0250924dd7017c83ef17335e6323ac5e27e65
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2023-11-09 20:41:04 +0530

    Revert "ensureSelectedThread: reduce time delay for fallback"
    This reverts commit d0ff0e3ace25e780479a837cf84c42b6caa41f05.

--- src/SwiftServer/Sources/SwiftServer/MessagesController.swift
+++ src/SwiftServer/Sources/SwiftServer/MessagesController.swift
@@ -380,7 +380,7 @@ final class MessagesController {
                     throw ErrorMessage("thread not selected")
                 }
             } catch {
-                if attempt > 2 { // ~100ms
+                if attempt > 5 { // 250ms
                     if let addresses = getToFieldAddresses(), addresses.contains(where: { isSameContact($0, addressToMatch) }) {
                         debugLog("ensureSelectedThread: used fallback")
                         return
