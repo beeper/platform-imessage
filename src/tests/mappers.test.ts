6df30c25bd4e28bfd95c20d44b04fe98e8e0c31e
slice <tinyslices@gmail.com>
2023-12-30 01:25:59 -0800

    tests: convert data uris to buffers before mapping
    The code expects them to be Buffer(-like) objects.

--- src/tests/mappers.test.ts
+++ src/tests/mappers.test.ts
@@ -6,6 +6,15 @@ import fixture2 from './fixture2.json'
 describe('multi part messages', () => {
   [fixture1, fixture2].forEach((f: any, i) => {
     test(String(i), () => {
+      const fixupDataUris = (object, key) => {
+        if (key in object) {
+          // Slice off "data:;base64," present in test fixtures.
+          object[key] = Buffer.from(object[key].slice(13), 'base64')
+        }
+      }
+
+      fixupDataUris(f[0], 'attributedBody')
+      fixupDataUris(f[0], 'message_summary_info')
       const msg = mapMessage(f[0], f[1], f[2], f[3])
       msg.forEach(m => delete m._original)
       expect(msg).toMatchSnapshot()
