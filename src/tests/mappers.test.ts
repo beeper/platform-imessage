7142c78c1762e9bffd173e0ae556be8676acb961
Skip R <tinyslices@gmail.com>
2024-01-02 21:29:34 -0800

    tests: add fixtures for partial unsends (#62)
    I also made some ergonomic changes to the testing code.

--- src/tests/mappers.test.ts
+++ src/tests/mappers.test.ts
@@ -1,23 +1,42 @@
 import './fix-env'
+import fs from 'fs/promises'
+import path from 'path'
+
 import { mapMessage } from '../mappers'
-import fixture1 from './fixture1.json'
-import fixture2 from './fixture2.json'
+
+async function testMessageMapFixture(fixturePath: string) {
+  const pathRelativeToTests = path.join(__dirname, fixturePath)
+
+  test(path.basename(fixturePath), async () => {
+    const parameters: Parameters<typeof mapMessage> = JSON.parse(await fs.readFile(pathRelativeToTests, 'utf8'))
+
+    const fixupDataUri = (key: string) => {
+      if (key in parameters[0]) {
+        // Slice off "data:;base64," present in test fixtures.
+        parameters[0][key] = Buffer.from(parameters[0][key].slice(13), 'base64')
+      }
+    }
+
+    fixupDataUri('attributedBody')
+    fixupDataUri('message_summary_info')
+
+    parameters[1] ??= []
+    parameters[2] ??= []
+
+    const message = mapMessage(...parameters)
+    message.forEach(m => delete m._original)
+    expect(message).toMatchSnapshot()
+  })
+}
 
 describe('multi part messages', () => {
-  [fixture1, fixture2].forEach((f: any, i) => {
-    test(String(i), () => {
-      const fixupDataUris = (object, key) => {
-        if (key in object) {
-          // Slice off "data:;base64," present in test fixtures.
-          object[key] = Buffer.from(object[key].slice(13), 'base64')
-        }
-      }
+  ['./fixture1.json', './fixture2.json'].forEach(testMessageMapFixture)
+})
 
-      fixupDataUris(f[0], 'attributedBody')
-      fixupDataUris(f[0], 'message_summary_info')
-      const msg = mapMessage(f[0], f[1], f[2], f[3])
-      msg.forEach(m => delete m._original)
-      expect(msg).toMatchSnapshot()
-    })
-  })
+describe('partially unsending messages', () => {
+  [
+    './partial_leading_unsends.json',
+    './partial_trailing_unsends.json',
+    './partial_multiple_middle_adjacent_unsend.json',
+  ].forEach(testMessageMapFixture)
 })
