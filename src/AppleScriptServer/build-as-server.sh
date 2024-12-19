601ce157cbf15a2105fe80a7719f8fbe9051800e
Tamás Zahola <tzahola@gmail.com>
2024-12-19 18:21:39 +0100

    make sure output folder exists

--- src/AppleScriptServer/build-as-server.sh
+++ src/AppleScriptServer/build-as-server.sh
@@ -9,5 +9,6 @@ strip Release/AppleScriptServer
 codesign -f -s - Release/AppleScriptServer
 ls -lah Release/AppleScriptServer
 
+mkdir -p ../../binaries/darwin-{x64,arm64}
 lipo Release/AppleScriptServer -thin x86_64 -output ../../binaries/darwin-x64/AppleScriptServer
 lipo Release/AppleScriptServer -thin arm64 -output ../../binaries/darwin-arm64/AppleScriptServer
