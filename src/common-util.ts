7223032669a58bf2bcad667e60fe5ba995965111
Skip R <tinyslices@gmail.com>
2025-04-08 17:24:43 -0700

    fix `isSelectable` types

--- src/common-util.ts
+++ src/common-util.ts
@@ -1,10 +1,10 @@
 import { isEmojiOrSpacesOnlyString } from '@textshq/platform-sdk/dist/emoji'
 import type { MessageWithExtra } from './mappers'
 
-export const isSelectable = (message: MessageWithExtra) =>
+export const isSelectable = (message: MessageWithExtra): boolean =>
   (!message.attachments?.length
     && !message.links?.length
     && !message.tweets?.length
     && typeof message.extra?.part === 'undefined'
-    && message.text
+    && message.text != null
     && !isEmojiOrSpacesOnlyString(message.text))
