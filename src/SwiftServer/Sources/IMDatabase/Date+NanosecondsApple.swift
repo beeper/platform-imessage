4b9673d1d477480d23dd1c1066b5dc35aa3bb1d9
Skip R <skip@beeper.com>
2025-05-21 14:00:24 -0700

    imdb: break out date helpers

---
+++ src/SwiftServer/Sources/IMDatabase/Date+NanosecondsApple.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+// imessage db uses "nanoseconds since reference date" for its dates
+extension Date {
+    var nanosecondsSinceReferenceDate: Int {
+        Int(timeIntervalSinceReferenceDate * 1_000_000_000)
+    }
+
+    init(nanosecondsSinceReferenceDate nanos: Int) {
+        self = Date(timeIntervalSinceReferenceDate: Double(nanos) / 1_000_000_000)
+    }
+}
