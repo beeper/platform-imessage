be78b220dfd171905999ee93f8f98c0b8a90ddb9
Skip R <skip@beeper.com>
2025-07-16 19:03:49 -0700

    persist: don't drop writes that occur while waiting for fs write
    it is pretty easy to fill up the task queue by e.g. archiving 100
    threads

--- src/persistence.ts
+++ src/persistence.ts
@@ -1,6 +1,7 @@
 import * as fs from 'node:fs/promises'
 import { Thread, ThreadReminder } from '@textshq/platform-sdk'
 import { AppleDate } from './time'
+/* eslint-disable no-void -- we are being explicit about "naked" promise calls */
 
 /** All persisted thread data. Stays in memory and gets (de)serialized from/to disk. */
 export type PersistedData = Record<Thread['id'], PersistedThreadProps>
@@ -26,7 +27,10 @@ export type PersistedBatchGetResults<P extends keyof PersistedThreadProps> = Rec
 export class Persistence {
   private data: PersistedData
 
-  private needsSaving = false
+  /** The current version number of the data. Used to prevent writes racing each other. */
+  private version = 0
+
+  private saveInFlight = false
 
   constructor(
     /** A callback used to persist the in-memory data to disk given a serialized string representation of all data. */
@@ -38,27 +42,47 @@ export class Persistence {
   }
 
   private invalidateSavedData() {
-    if (this.needsSaving) return
-    this.needsSaving = true;
+    this.version++
 
-    (async () => {
-      if (texts.IS_DEV) {
-        texts.log('imsg: persistence is going to save data')
-      }
-      if (!this.needsSaving) {
-        // we don't expect concurrent instances of this microtask due to the
-        // boolean, but check this anyways in order to prevent corruption
-        texts.error('imsg: `needsSaving` became false somehow, something very sneaky is going on')
-        return
-      }
+    if (!this.saveInFlight) {
+      // we have new changes and we aren't already in the middle of saving, so
+      // enqueue a save
+      this.saveInFlight = true
+      void this.save(this.version)
+    } else {
+      // we have new changes in-memory, but we are still in the middle of
+      // writing out to disk. since `actuallySave` checks for outstanding
+      // writes that happened while it was busy (via
+      // `version`/`lastSavedVersion`), do nothing here
+    }
+  }
 
-      try {
-        await this.saver(JSON.stringify(this.data))
-      } catch (error) {
-        texts.error(`imsg: couldn't persist data, any changes will be lost: ${String(error)}`)
-      }
-      this.needsSaving = false
-    })()
+  private async save(versionToWrite: number) {
+    if (texts.IS_DEV) {
+      texts.log(`imsg: persistence is going to save data (${versionToWrite})`)
+    }
+
+    try {
+      await this.saver(JSON.stringify(this.data))
+    } catch (error) {
+      texts.error(`imsg: couldn't persist data (${versionToWrite}), any changes will be lost: ${String(error)}`)
+
+      // don't do the version check, because if the write fails again we'd just
+      // hammer retries infinitely -- at this point, just try writing when/if
+      // the data changes next time (also, this early exit still sets
+      // `saveInFlight` to `false`)
+      return
+    } finally {
+      this.saveInFlight = false
+    }
+
+    // check if the in-memory data was invalidated while we were writing, and
+    // schedule another write if so
+    const versionAfterSaving = this.version
+    if (versionAfterSaving > versionToWrite) {
+      if (texts.IS_DEV) texts.log(`imsg: witnessed version changed to ${versionAfterSaving} (from ${versionToWrite}) after done writing, queueing another save`)
+      void this.save(versionAfterSaving)
+    }
   }
 
   /**
