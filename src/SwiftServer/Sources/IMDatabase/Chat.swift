338d08493eadf89b9adfa3f61b9515b250d675cd
Skip R <skip@beeper.com>
2025-08-06 16:37:26 -0700

    imd: add `Chat.isBusiness`

--- src/SwiftServer/Sources/IMDatabase/Chat.swift
+++ src/SwiftServer/Sources/IMDatabase/Chat.swift
@@ -3,6 +3,17 @@ public struct Chat {
     public var id: Int
     public var guid: String
 
-    /** The name of this chat, if it's a group. */
+    /** For group chats, a custom name. For business chats, the business name. */
     public var displayName: String?
+}
+
+private var businessGUIDPrefixes: [String] {
+    // RCS might not be a thing, but just in case
+    ["SMS;-;urn:biz:", "iMessage;-;urn:biz:", "RCS;-;urn:biz:"]
+}
+
+public extension Chat {
+    var isBusiness: Bool {
+        businessGUIDPrefixes.contains(where: { guid.hasPrefix($0) })
+    }
 }
