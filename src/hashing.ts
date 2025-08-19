17324147ecec2a8d1955e449dbc5d3a222333c3b
Skip R <skip@beeper.com>
2025-08-18 17:40:21 -0700

    accept more valid alphanumeric sender ids
    after asking around, seems like these sender ids pretty frequently
    contain spaces and or dashes
    
    speculative fix for DESK-15234

--- src/hashing.ts
+++ src/hashing.ts
@@ -16,7 +16,8 @@ interface Threadlike {
 const { hashers } = swiftServer
 
 const entirelyNumbersAndSymbols = /^[\d\s+\-()]+$/
-const entirelyAlphanumericSenderID = /^[\da-zA-Z-]{1,11}$/
+// https://web.archive.org/web/20250624231541/https://api.support.vonage.com/hc/en-us/articles/204017783-United-Kingdom-SMS-Features-and-Restrictions
+const entirelyAlphanumericSenderID = /^[\da-zA-Z .&_/-]{1,11}$/
 
 // https://en.wikipedia.org/wiki/Mobile_marketing#Custom_Sender_ID
 function likelyAlphanumericSenderID(id: string): boolean {
