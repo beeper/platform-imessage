bcd89e6def51c9ac310d676677ad448dcc043be4
Purav Manot <manot.purav@gmail.com>
2026-03-19 16:19:12 +0530

    use a precompiled script instead of recompiling every time (wip)

--- src/SwiftServer/Sources/SwiftServer/OSA.swift
+++ src/SwiftServer/Sources/SwiftServer/OSA.swift
@@ -1,3 +1,4 @@
+import Foundation
 import OSAKit
 import SwiftServerFoundation
 import Logging
@@ -5,34 +6,111 @@ import Logging
 private let log = Logger(swiftServerLabel: "osa")
 
 enum OSA {
-    private static func run(_ source: String, language: OSALanguage? = .init(forName: "JavaScript")) throws {
-        let script = OSAScript(source: source, language: language)
-        var scriptError: NSDictionary?
-        let _ = script.executeAndReturnError(&scriptError)
-        if let scriptError { throw ErrorMessage(String(describing: scriptError)) }
+    private static let scriptExecutionLock = UnfairLock()
+
+    private static let messagesScriptResult: Result<OSAScript, Error> = Result {
+        let messagesScript = try makeCompiledScript(
+            source: """
+            const messagesApplication = Application('/System/Applications/Messages.app')
+
+            function chatForThreadID(threadID) {
+                const chat = messagesApplication.chats.byId(threadID)()
+                if (!chat) {
+                    throw new Error(`Could not find Messages chat for thread ID: ${threadID}`)
+                }
+                return chat
+            }
+
+            function sendText(threadID, text) {
+                const chat = chatForThreadID(threadID)
+                messagesApplication.send(text, { to: chat })
+            }
+
+            function sendFile(threadID, filePath) {
+                const chat = chatForThreadID(threadID)
+                messagesApplication.send(Path(filePath), { to: chat })
+            }
+            """,
+            languageName: "JavaScript"
+        )
+        log.debug("compiled cached Messages OSA script")
+        return messagesScript
+    }
+
+    private static let automationAccessPromptScriptResult: Result<OSAScript, Error> = Result {
+        let automationAccessPromptScript = try makeCompiledScript(
+            source: """
+            on promptAutomationAccess()
+                tell application "Messages" to set automationAccessPromptAccounts to accounts
+            end promptAutomationAccess
+            """,
+            languageName: "AppleScript"
+        )
+        log.debug("compiled cached automation access OSA script")
+        return automationAccessPromptScript
     }
 
     static func send(threadID: String, text: String) throws {
-        try run("""
-        const [tid, txt] = \(try jsonStringify([threadID, text]))
-        const Messages = Application('/System/Applications/Messages.app')
-        const to = Messages.chats.byId(tid)()
-        Messages.send(txt, { to })
-        """)
+        try executeHandler(
+            script: messagesScriptResult.get(),
+            handlerName: "sendText",
+            arguments: [threadID, text]
+        )
     }
 
     static func send(threadID: String, filePath: String) throws {
-        try run("""
-        const [tid, fp] = \(try jsonStringify([threadID, filePath]))
-        const Messages = Application('/System/Applications/Messages.app')
-        const to = Messages.chats.byId(tid)()
-        Messages.send(Path(fp), { to })
-        """)
+        try executeHandler(
+            script: messagesScriptResult.get(),
+            handlerName: "sendFile",
+            arguments: [threadID, filePath]
+        )
     }
 
     static func promptAutomationAccess() throws {
-        try run("""
-        tell application "Messages" to set a to accounts
-        """, language: .init(forName: "AppleScript"))
+        try executeHandler(
+            script: automationAccessPromptScriptResult.get(),
+            handlerName: "promptAutomationAccess",
+            arguments: []
+        )
+    }
+
+    private static func executeHandler(
+        script: OSAScript,
+        handlerName: String,
+        arguments: [Any]
+    ) throws {
+        try scriptExecutionLock.withLock {
+            try autoreleasepool {
+                var scriptErrorInfo: NSDictionary?
+                let _ = script.executeHandler(withName: handlerName, arguments: arguments, error: &scriptErrorInfo)
+                if let scriptErrorInfo {
+                    throw makeErrorMessage(from: scriptErrorInfo)
+                }
+            }
+        }
+    }
+
+    private static func makeCompiledScript(source: String, languageName: String) throws -> OSAScript {
+        guard let language = OSALanguage(forName: languageName) else {
+            throw ErrorMessage("\(languageName) OSA language is unavailable")
+        }
+
+        let script = OSAScript(source: source, language: language)
+        var scriptErrorInfo: NSDictionary?
+        let didCompile = script.compileAndReturnError(&scriptErrorInfo)
+        if !didCompile || scriptErrorInfo != nil {
+            throw makeErrorMessage(from: scriptErrorInfo ?? NSDictionary())
+        }
+        return script
+    }
+
+    private static func makeErrorMessage(from scriptErrorInfo: NSDictionary) -> ErrorMessage {
+        if let detailedMessage = scriptErrorInfo[OSAScriptErrorMessageKey] as? String {
+            return ErrorMessage(detailedMessage)
+        }
+        if let briefMessage = scriptErrorInfo[OSAScriptErrorBriefMessageKey] as? String {
+            return ErrorMessage(briefMessage)
+        }
+        return ErrorMessage(String(describing: scriptErrorInfo))
     }
 }
