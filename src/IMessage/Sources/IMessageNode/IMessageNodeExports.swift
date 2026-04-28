de1bad9b3bf3101dc12a0cefd774f3722d817636
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2026-04-28 23:22:15 +0530

    inline IMessageNodeEventBridge.setEventCallback

--- src/IMessage/Sources/IMessageNode/IMessageNodeExports.swift
+++ src/IMessage/Sources/IMessageNode/IMessageNodeExports.swift
@@ -52,7 +52,19 @@ import IMessageCore
         },
 
         "setEventCallback": NodeFunction { (onEvent: NodeFunction) in
-            try IMessageNodeEventBridge.setEventCallback(onEvent)
+            let eventQueue = try NodeAsyncQueue(label: "event-watcher-events")
+            let sentryQueue = try? NodeAsyncQueue(label: "event-watcher-sentry")
+            let onEvent = SendableBox(onEvent)
+            IMessageHost.setEventCallback { events in
+                try eventQueue.run {
+                    let nodeEvents = try NodeBridgeUtilities.nodeArray(from: events.map { $0.jsonObject() })
+                    try onEvent.value.call([nodeEvents])
+                }
+            } reportToSentry: { message in
+                try? sentryQueue?.run {
+                    try Node.texts.Sentry.captureMessage(message)
+                }
+            }
             return // needed to resolve a compile-time type ambiguity apparently
         },
 
