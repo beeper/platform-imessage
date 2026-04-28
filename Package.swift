1c6b27f7f5b17f9d1fe95930321475ff9dbdcfcb
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2026-04-28 19:19:23 +0530

    patch for xcode 26.4.1

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ var dependencies: [Package.Dependency] = [
     .package(url: "https://github.com/apple/swift-collections.git", from: "1.2.0"),
     .package(url: "https://github.com/apple/swift-async-algorithms", from: "1.0.0"),
     .package(url: "https://github.com/apple/swift-argument-parser", from: "1.6.1"),
-    .package(url: "https://github.com/swiftlang/swift-syntax.git", exact: "601.0.1"),
+    .package(url: "https://github.com/swiftlang/swift-syntax.git", exact: "603.0.0-prerelease-2025-10-30"),
 ]
 
 var targets: [Target] = [
