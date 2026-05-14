274132d855ce2a76f59f979311ea45a291f74b3d
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2026-05-14 19:31:19 +0530

    -

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -3,7 +3,6 @@ import { installCliGlobals } from './bootstrap'
 const IS_DEV = process.env.NODE_ENV !== 'production'
 const isLoggingEnabled = process.argv.includes('--verbose')
 
-process.env.IMESSAGE_STRIP_INTERNAL_FIELDS ??= '1'
 installCliGlobals(IS_DEV, isLoggingEnabled)
 
 await import('./main')
